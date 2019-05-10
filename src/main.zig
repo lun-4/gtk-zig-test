98e5bc9dcbacdcc2f1b12bca9e030ef40327f666
Luna <git@l4.pm>
2019-05-10 01:56:08 -0300

    add explanations on main.zig

--- src/main.zig
+++ src/main.zig
@@ -8,19 +8,23 @@ const c = @cImport({
 // std.os.args, but i'd rather do this instead.
 
 export fn main(argc_val: c_int, argv_val: [*c][*c]u8) i32 {
+    // deconsting argc and argv or else zig's compiler will yell about
+    // the gtk_init call the deconst.
     var argc = argc_val;
     var argv = argv_val;
 
-    //std.debug.warn("argc type: {}\n", @typeName(@typeOf(argc)));
-    //std.debug.warn("argv type: {}\n", @typeName(@typeOf(argv)));
-
+    // heavy reliance on c pointers for this to work
     var window: [*c]c.GtkWidget = undefined;
 
     c.gtk_init(&argc, &argv);
 
+    // note that c.GTK_WINDOW_TOPLEVEL won't work due to it becoming a
+    // comptime_int instead of a member of the enum. we have to reference it
+    // by the enum itself first.
     window = c.gtk_window_new(c.GtkWindowType.GTK_WINDOW_TOPLEVEL);
 
     _ = c.gtk_widget_show(window);
     _ = c.gtk_main();
+
     return 0;
 }
