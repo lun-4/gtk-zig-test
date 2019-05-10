5139a8be5c38448d2126f36334fb584852b761e0
Luna <git@l4.pm>
2019-05-10 01:46:00 -0300

    fix GTK_WINDOW_TOPLEVEL and argc types (almost)

--- src/main.zig
+++ src/main.zig
@@ -7,12 +7,15 @@ const c = @cImport({
 // we can try to not use export fn main and instead run with
 // std.os.args, but i'd rather do this instead.
 
-export fn main(argc: usize, argv: [*c][255]u8) i32 {
+export fn main(argc_val: c_int, argv: [*c][255]u8) i32 {
+    const argc = argc_val;
+    std.debug.warn("argc type: {}", @typeName(@typeOf(argc)));
     var window: [*c]c.GtkWidget = undefined;
 
     c.gtk_init(&argc, &argv);
 
-    window = c.gtk_window_new(c.GTK_WINDOW_TOPLEVEL);
+    window = c.gtk_window_new(c.GtkWindowType.GTK_WINDOW_TOPLEVEL);
+
     _ = c.gtk_widget_show(window);
     _ = c.gtk_main();
     return 0;
