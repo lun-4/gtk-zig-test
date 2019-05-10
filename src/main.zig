0a4f453f8965959d6d9c71da22f86d7f941985aa
Luna <git@l4.pm>
2019-05-10 01:50:35 -0300

    deconst argv as well

--- src/main.zig
+++ src/main.zig
@@ -7,9 +7,13 @@ const c = @cImport({
 // we can try to not use export fn main and instead run with
 // std.os.args, but i'd rather do this instead.
 
-export fn main(argc_val: c_int, argv: [*c][255]u8) i32 {
-    const argc = argc_val;
-    std.debug.warn("argc type: {}", @typeName(@typeOf(argc)));
+export fn main(argc_val: c_int, argv_val: [*c][*c]u8) i32 {
+    var argc = argc_val;
+    var argv = argv_val;
+
+    //std.debug.warn("argc type: {}\n", @typeName(@typeOf(argc)));
+    //std.debug.warn("argv type: {}\n", @typeName(@typeOf(argv)));
+
     var window: [*c]c.GtkWidget = undefined;
 
     c.gtk_init(&argc, &argv);
