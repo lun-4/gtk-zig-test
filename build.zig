ac1f8f80fa4e4f2a26dc1a11365ebdf1c98a126d
Luna <git@l4.pm>
2019-05-10 01:50:53 -0300

    remove unusable comment

--- build.zig
+++ build.zig
@@ -5,8 +5,6 @@ pub fn build(b: *Builder) void {
     const exe = b.addExecutable("gtk-zig-test", "src/main.zig");
     exe.setBuildMode(mode);
 
-    //-lgtk-3 -lgdk-3 -lpangocairo-1.0 -lpango-1.0 -latk-1.0 -lcairo-gobject -lcairo -lgdk_pixbuf-2.0 -lgio-2.0 -lgobject-2.0 -lglib-2.0
-
     // TODO: call pkg-build
     const to_link = [][]const u8{
         "c",
